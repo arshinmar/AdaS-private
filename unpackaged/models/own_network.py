2fbf7dc60901a74ac416a3fbba3ea24ef42a16c7
arshinmar <arsh.kadakia18@gmail.com>
2020-07-27 18:11:37 -0400

    updated to size=96

--- unpackaged/models/own_network.py
+++ unpackaged/models/own_network.py
@@ -124,11 +124,11 @@ class Network(nn.Module):
         self.shortcut_3_index = 21 #Number on excel corresponding to shortcut 2
         self.shortcut_4_index = 28
         ####################### O% ########################
-        self.superblock1_indexes=[32,32,32,32,32,32,32]
-        self.superblock2_indexes=[32,32,32,32,32,32]
-        self.superblock3_indexes=[32,32,32,32,32,32]
-        self.superblock4_indexes=[32,32,32,32,32,32]
-        self.superblock5_indexes=[32,32,32,32,32,32]
+        self.superblock1_indexes=[96,96,96,96,96,96,96]
+        self.superblock2_indexes=[96,96,96,96,96,96]
+        self.superblock3_indexes=[96,96,96,96,96,96]
+        self.superblock4_indexes=[96,96,96,96,96,96]
+        self.superblock5_indexes=[96,96,96,96,96,96]
 
         #self.superblock1_indexes=[64, 2, 64, 2, 64, 2, 64]
         #self.superblock2_indexes=[2, 128, 2, 128, 2, 128]
