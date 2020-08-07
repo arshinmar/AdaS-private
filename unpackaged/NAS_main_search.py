47d81ad110f82a5fee8e70229a8f6fbecb3ff943
arshinmar <arsh.kadakia18@gmail.com>
2020-08-07 12:11:17 -0400

    Update NAS_main_search.py

--- unpackaged/NAS_main_search.py
+++ unpackaged/NAS_main_search.py
@@ -59,14 +59,14 @@ if __name__ == '__main__':
     if not os.path.exists(output_path_string_full_train):
         os.mkdir(output_path_string_full_train)
 
-    conv_data,rank_final_data,rank_stable_data,output_sizes,delta_info=run_trials(train_loader,test_loader,device,optimizer,scheduler,epochs,output_path_train)
-    create_trial_data_file(conv_data,delta_info,rank_final_data,rank_stable_data,output_path_string_trials,output_path_string_graph_files,output_path_string_modelweights)
+    #conv_data,rank_final_data,rank_stable_data,output_sizes,delta_info=run_trials(train_loader,test_loader,device,optimizer,scheduler,epochs,output_path_train)
+    #create_trial_data_file(conv_data,delta_info,rank_final_data,rank_stable_data,output_path_string_trials,output_path_string_graph_files,output_path_string_modelweights)
     print('Done Trials.')
 
     #run_saved_weights_full_train(train_loader,test_loader,device,output_sizes,range(0,250),output_path_fulltrain)
     #Note Last Iter not used
     #output_sizes=[[40,40,40,58,40,42,40],[252,90,142,90,78,90],[388,174,198,174,116,174],[334,116,176, 116, 82, 116],[388,30,40,30,44,30]]
-
+    output_sizes=[[58,46,58,60,58,64,58],[388,106,158,106,164,106],[388,258,388,258,186,258],[388,216,334,216,156,216],[388,72,40,72,54,72]]
     run_fresh_full_train(train_loader,test_loader,device,output_sizes,full_train_epochs,output_path_fulltrain)
     create_full_data_file(GLOBALS.NET,output_path_string_full_train+'\\'+f"AdaS_last_iter_fulltrain_trial=0_net={GLOBALS.CONFIG['network']}_dataset={GLOBALS.CONFIG['dataset']}.xlsx",
                                  output_path_string_full_train+'\\'+f"AdaS_fresh_fulltrain_trial=0_net={GLOBALS.CONFIG['network']}_dataset={GLOBALS.CONFIG['dataset']}.xlsx",
